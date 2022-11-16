cbf5b82240c6bcf96d52f912bc4326b37eac8ad5
Avik Saha <avik.s@alfanar.com>
2022-11-16 04:47:08 +0000

    Grade Added in the Panel Header

--- webapp/utils/formatter.js
+++ webapp/utils/formatter.js
@@ -20,28 +20,37 @@ sap.ui.define([
                 11: "Dec"
             }
             const sMonth = oDates[oDate.getMonth()];
-            const sRetrDate = oDate.getUTCDate().toLocaleString().length === 1 ? `0${oDate.getUTCDate()}` : oDate.getUTCDate();    
-            const sYear = oDate.getFullYear();     
-            sCalculatedDate = `${sMonth} ${sRetrDate}, ${sYear}`   
+            const sRetrDate = oDate.getUTCDate().toLocaleString().length === 1 ? `0${oDate.getUTCDate()}` : oDate.getUTCDate();
+            const sYear = oDate.getFullYear();
+            sCalculatedDate = `${sMonth} ${sRetrDate}, ${sYear}`
 
             return sCalculatedDate;
 
         },
 
-        getMicroChartValueColor: function(sPercentage){
+        getMicroChartValueColor: function (sPercentage) {
             const fPercentage = parseFloat(sPercentage);
             let sValueColor;
-            if(fPercentage > 99){
+            if (fPercentage > 99) {
                 sValueColor = "Good";
-            }else {
+            } else {
                 sValueColor = "Error"
             }
 
             return sValueColor;
         },
-        getPercentageForMicroChart: function(sPercentage){
+        getPercentageForMicroChart: function (sPercentage) {
             const fPercentage = parseFloat(sPercentage);
-            return fPercentage;                 
+            return fPercentage;
         },
+        getGradeFormatted: (sGrade) => {
+            let sFormattedGrade;
+            if (sGrade) {
+                sFormattedGrade = sGrade
+            } else {
+                sFormattedGrade = "NA"
+            }
+            return sFormattedGrade;
+        }
     }
 });
